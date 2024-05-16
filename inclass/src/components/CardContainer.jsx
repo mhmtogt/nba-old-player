c5d0379285e321df86a32e582fe1ec231db3ff4a
Mehmet OGUT <mogut44949@gmail.com>
2024-05-16 13:38:09 +0300

    render form

--- inclass/src/components/CardContainer.jsx
+++ inclass/src/components/CardContainer.jsx
@@ -5,32 +5,38 @@ import PlayerCard from './PlayerCard';
 
 const CardContainer = () => {
     const [search,setSearch]=useState(" ")
-    console.log(search)
-    const showImage=true;// burası ekrana basılacak olan kartların true ise logo false ise detayları göstermek için alt componente 
-    const filteredData=data.filter(item=>item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))//includes(search)  item içerisinde yazılan name kontrol eder ve var mı yok mu ona bakra
+    // console.log(search)
+    // const showImage=true;// burası ekrana basılacak olan kartların true ise logo false ise detayları göstermek için alt componente 
+    // const filteredData=data.filter(item=>item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))//includes(search)  item içerisinde yazılan name kontrol eder ve var mı yok mu ona bakra
 
   return (
-    <div>
+    <>
     <Form.Control 
     placeholder= "search a player"
     className="w-50 m-auto " 
     onChange={(e)=>setSearch(e.target.value)}/> 
 
-    <Container className='rounded-4 my-4 p-3 card-container'>
+    <Container className='rounded-4 my-4 p-3 card-container '>
         <Row className="justify-content-center g-3">
-        {filteredData.map((player,i)=>(
-            <Col key={i} xl={3} lg={4} md={6}>
+
+          {data.filter((item)=>item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase().trim())).map((player,i)=>(
+          <Col id={i}  xl={3} lg={4} md={6}>
+          <PlayerCard {...player} />burada 3 nokta player "playere aç ve içindeki tüm bilgileri props olarak gönder demek "
+         </Col>))}
+
+        {/* {filteredData.map((player,i)=>(
+            <Col id={i}  xl={3} lg={4} md={6}>
             <PlayerCard {...player} showImage={showImage}/>burada 3 nokta player "playere aç ve içindeki tüm bilgileri props olarak gönder demek "
            </Col>
         ))}
-       {/* {data.map((player,i)=>(
+       {data.map((player,i)=>(
             <Col key={i} xl={3} lg={4} md={6}>
             <PlayerCard {...player} showImage={showImage}/>burada 3 nokta player "playere aç ve içindeki tüm bilgileri props olarak gönder demek "
            </Col>
         ))} */}
         </Row>
     </Container>
-    </div>
+    </>
   )
 }
 
